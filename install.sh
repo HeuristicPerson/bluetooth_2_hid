a1423e33152aeb356f72c9b19d81d8a8a1e99bdf
appellemoipolo <appellemoipolo@gmail.com>
2022-02-18 23:36:39 +0100

    Initialize install script and systemd service

---
+++ install.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
+echo "dwc2" | sudo tee -a /etc/modules
+echo "libcomposite" | sudo tee -a /etc/modules
+
+touch /usr/bin/isticktoit_usb
+chmod +x /usr/bin/isticktoit_usb
+
+sed -i '/^exit/i /usr/bin/isticktoit_usb' /etc/rc.local
+
+printf’#!/bin/bash
+cd /sys/kernel/config/usb_gadget/
+mkdir -p isticktoit
+cd isticktoit
+echo 0x1d6b > idVendor # Linux Foundation
+echo 0x0104 > idProduct # Multifunction Composite Gadget
+echo 0x0100 > bcdDevice # v1.0.0
+echo 0x0200 > bcdUSB # USB2
+mkdir -p strings/0x409
+echo "fedcba9876543210" > strings/0x409/serialnumber
+echo "Tobias Girstmair" > strings/0x409/manufacturer
+echo "iSticktoit.net USB Device" > strings/0x409/product
+mkdir -p configs/c.1/strings/0x409
+echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
+echo 250 > configs/c.1/MaxPower
+# Add functions here
+mkdir -p functions/hid.usb0
+echo 1 > functions/hid.usb0/protocol
+echo 1 > functions/hid.usb0/subclass
+echo 8 > functions/hid.usb0/report_length
+echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
+ln -s functions/hid.usb0 configs/c.1/
+# End functions
+ls /sys/class/udc > UDC’ > /usr/bin/isticktoit_usb
+
+cd ~
+git clone https://github.com/HeuristicPerson/bluetooth_2_hid
+
+cd bluetooth_2_hid
+
+#sudo cp bluetooth_2_hid.service /etc/systemd/system/
+#sudo chown root:root /etc/systemd/system/bluetooth_2_hid.service
+#sudo chmod 644 /etc/systemd/system/bluetooth_2_hid.service
+
+sudo ln -s /home/pi/bluetooth_2_hid/bluetooth_2_hid.service /etc/systemd/system/
+
+sudo pip3 install evdev
+
+
