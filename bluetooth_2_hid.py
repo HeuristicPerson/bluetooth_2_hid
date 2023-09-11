f59d2416883f0065a307e0cd6fc1e0271c3bce8e
quaxalber <64684396+quaxalber@users.noreply.github.com>
2023-09-11 21:50:20 +0200

    Complete rewrite. Now supports keyboard&mouse

--- bluetooth_2_hid.py
+++ bluetooth_2_hid.py
@@ -1,245 +1,88 @@
 #!/usr/bin/env python
+#
+# Reads incoming mouse and keyboard events (e.g., Bluetooth) and forwards them to USB using Linux gadget mode 
+#
+import usb_hid
+from select import select
+from evdev import InputDevice, ecodes
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.mouse import Mouse
+from evdev_2_hid import Converter
 
-"""
-Author:     @PixelGordo
-Date:       2021-12-15
-Version:    0.2
-References: https://www.isticktoit.net/?p=1383
-            https://github.com/mikerr/pihidproxy
-========================================================================================================================
-Script that reads the inputs of one keyboard (typically you would like to read a bluetooth keyboard) and translates them
-to HID proxy commands through the USB cable. This way you can have the connections below:
+class ComboDeviceHidProxy:
+    def __init__(self, keyboard_in: str, mouse_in: str):
+        self.keyboard_in = InputDevice(keyboard_in)
+        self.keyboard_out = Keyboard(usb_hid.devices) 
+        self.mouse_in = InputDevice(mouse_in)
+        self.mouse_out =  Mouse(usb_hid.devices)
+        self.converter = Converter()
+        self.input_devices = {dev.fd: dev for dev in [self.keyboard_in, self.mouse_in] if dev is not None}
 
-    Keyboard --(bluetooth)--> Raspberry Pi Zero W --(usb)--> Computer
+    def handle_key(self, event):
+        if self.is_mouse_button(event):
+            self.handle_mouse_button(event)
+        else:
+            self.handle_keyboard_key(event) 
 
-The final result is the computer (on the right) receives inputs as if they were coming from an USB keyboard. The
-advantages of this technique are:
+    def handle_keyboard_key(self, event):
+        key = self.converter.to_hid_key(event.code) 
 
-    - You can use your bluetooth keyboard on any device that has USB connection, no bluetooth drivers or software is
-      required.
-    - You can use your bluetooth keyboard in the BIOS or GRUB menu.
-    - You can use any bluetooth keyboard, even when they require a pairing password. You must do the pairing between
-      the Raspberry Pi and the bluetooth using that password but after that, you don't need to repeat the process, the
-      raspberry pi and the keyboard will remain paired even when you connect the Raspberry Pi to a different computer
-      (similar to what happens with the typical USB dongles that come with Logitech mouses).
-    - ...not implemented, but you could create your own macros in this script that would work in any computer you plug
-      the keyboard+raspberry. I'm thinking about Ctr-Shift-L to automatically send the keys of Lorem Ipsum...
-"""
+        if event.value == 0:
+            self.keyboard_out.release(key)
+        elif event.value == 1:
+            self.keyboard_out.press(key)     
 
-import argparse
-import os
-import sys
-import time
-import atexit
+    def handle_mouse_button(self, event):
+        button = self.converter.to_hid_mouse_button(event)
+ 
+        if event.value == 0:
+            self.mouse_out.release(button)
+        elif event.value == 1:
+            self.mouse_out.press(button)
 
-import evdev
+    def is_mouse_button(self, event):
+        return event.code in [ecodes.BTN_LEFT, ecodes.BTN_RIGHT, ecodes.BTN_MIDDLE]
 
-from libs import hid_codes
-from libs import keyboard
+    def move_mouse(self, event):
+        x, y, mwheel = 0
 
-# READ THIS: https://www.isticktoit.net/?p=1383
+        if event.code == ecodes.REL_X:
+            x = event.value
+        elif event.code == ecodes.REL_Y:
+            y = event.value
+        elif event.code == ecodes.REL_WHEEL:
+            mwheel = event.value
 
-# Constants
-#=======================================================================================================================
-u_INPUT_DEV = u'/dev/input/event0'   # In the Raspberry Pi Zero (apparently)
-u_OUTPUT_DEV = u'/dev/hidg0'
-u_LOG_FILE = u'/tmp/blu2hid.log'
+        self.mouse_out.move(x, y, mwheel)
+        
+    def combined_event_loop(self):
+        while True:
+            ready_devices = self.get_ready_devices()
+            for device in ready_devices:
+                self.read_input_events(device)
 
+    def get_ready_devices(self):
+        device_fds = select(self.input_devices, [], [])[0]
+        return (self.input_devices[fd] for fd in device_fds)
+    
+    def read_input_events(self, device):
+        if device == self.keyboard_in:
+            self.read_keyboard_events()
+        else:
+            self.read_mouse_events()
 
-# Helper Functions
-#=======================================================================================================================
-def _get_cmd_args():
-    """
-    Function to get command line arguments.
-    :return:
-    """
-    # [1/?] Defining the parser
-    #--------------------------
-    o_parser = argparse.ArgumentParser()
-    o_parser.add_argument('-i',
-                          action='store',
-                          default=u_INPUT_DEV,
-                          help='Input device (Bluetooth Keyboard). By default it\'s "/dev/input/event0"')
-    o_parser.add_argument('-o',
-                          action='store',
-                          default=u_OUTPUT_DEV,
-                          help='Output device (HID controller). By default it\'s "/dev/hidg0"')
-    o_parser.add_argument('-d',
-                          action='store_true',
-                          default=False,
-                          help='Debug mode ON. It\'ll print on screen information about the input keys detected and the'
-                               'output HID command sent')
-    o_parser.add_argument('-t',
-                          action='store_true',
-                          default=False,
-                          help='Test mode ON. The program will read the inputs normally but it won\'t generate the'
-                               'output HID commands. This mode is useful to check that everything is working fine apart'
-                               'from the actual HID command delivery')
-    o_parser.add_argument('-l',
-                          action='store_true',
-                          default=False,
-                          help='Log mode ON. The program will write a log file "/tmp/bluetooth2hid.log" containing any'
-                               'unknown input key')
+    def read_keyboard_events(self):
+        for event in self.keyboard_in.read():
+            if event.type == ecodes.EV_KEY and event.value < 2:
+                self.handle_key(event)
 
-    o_parsed_data = o_parser.parse_args()
+    def read_mouse_events(self):
+        for event in self.mouse_in.read():
+            if event.type == ecodes.EV_KEY and event.value < 2:
+                self.handle_key(event)
+            elif event.type == ecodes.EV_REL:
+                self.move_mouse(event)
 
-    # [2/?] Validation of the command line arguments
-    #-----------------------------------------------
-    # I validate the options first because depending on their values, we can relax the requirements on the output
-    b_mode_debug = o_parsed_data.d
-    b_mode_test = o_parsed_data.t
-    b_mode_log = o_parsed_data.l
-
-    print('[ pass ]    Debug mode: %s' % b_mode_debug)
-    print('[ pass ]     Test mode: %s' % b_mode_test)
-    print('[ pass ]      Log mode: %s (%s)' % (b_mode_log, u_LOG_FILE))
-
-    # Input device
-    #-------------
-    # I'm not sure about this validation because I don't know what happens when you first connect the Bluetooth without
-    # any activity on the keyboard. If the keyboard doesn't activate, this validation will fail. Anyway, once the
-    # program is running, the main loop is able to keep trying the connection if it's not found.
-    u_input = str(o_parsed_data.i)
-    #if not (os.access(u_input, os.F_OK) and os.access(u_input, os.R_OK)):
-    #    print('[ FAIL ]  Input device: %s (can\'t be read or is not found)' % u_input)
-    #    sys.exit()
-    #else:
-    #    print('[ pass ]  Input device: %s' % u_input)
-    print('[ pass ]  Input device: %s' % u_input)
-
-    # Output device
-    #--------------
-    u_output = str(o_parsed_data.o)
-    #if not (os.access(u_input, os.F_OK) and os.access(u_input, os.W_OK)):
-    #    if b_mode_test:
-    #        print('[ INFO ] Output device: %s (can\'t be read or is not found)' % u_output)
-    #    else:
-    #        print('[ FAIL ] Output device: %s (can\'t be read or is not found)' % u_output)
-    #        sys.exit()
-    #else:
-    #    print('[ pass ] Output device: %s' % u_output)
-    print('[ pass ] Output device: %s' % u_output)
-
-    return {'u_input': u_input,
-            'u_output': u_output,
-            'b_mode_debug': b_mode_debug,
-            'b_mode_test': b_mode_test,
-            'b_mode_log': b_mode_log}
-
-
-def _get_input_device(pu_device):
-    """
-    Function to get the device object.
-    :param pu_device:
-    :return: The device object.
-    """
-
-    o_device = None
-    while o_device is None:
-        try:
-            o_device = evdev.InputDevice(pu_device)
-        except OSError:
-            print('[ WAIT ] Opening Bluetooth input (%s)...' % u_INPUT_DEV)
-            time.sleep(3)
-
-    print('[ pass ] Bluetooth input open (%s)' % str(pu_device))
-    return o_device
-
-
-def _get_output_device(pu_device):
-    """
-    Function to get the output object.
-    :param pu_device:
-    :return: The device object.
-    """
-
-    o_device = None
-    while o_device is None:
-        try:
-            o_device = open(pu_device, 'wb+', buffering=0)
-        except OSError:
-            print('[ WAIT ] Opening HID output (%s)...' % u_OUTPUT_DEV)
-            time.sleep(3)
-
-    print('[ pass ] HID output open (%s)' % str(pu_device))
-    return o_device
-
-
-def _get_devices(pu_input, pu_output, po_input, po_output):
-    """
-    Function to get both devices, input and output.
-    :param pu_input:
-    :param pu_output:
-    :return:
-    """
-    pass
-
-def _grab_device(device):
-    device.grab()
-
-def _ungrab_device(device):
-    device.ungrab()
-
-# Main Code
-#=======================================================================================================================
-if __name__ == '__main__':
-    print('Bluetooth to HID events translator v0.1 2019-01-29')
-    print('==================================================')
-
-    o_args = _get_cmd_args()
-
-    # Initialization
-    #---------------
-    is_debug_enabled = bool(o_args['b_mode_debug'])
-    is_test_mode_enabled = bool(o_args['b_mode_test'])
-    o_hid_keyboard = keyboard.HidKeyboard()
-    o_input_device = _get_input_device(o_args['u_input'])
-    _grab_device(o_input_device)
-    atexit.register(_ungrab_device, device = o_input_device)
-    o_output_device = _get_output_device(o_args['u_output'])
-
-    # Main loop
-    #----------
-    while True:
-        try:
-            for o_event in o_input_device.read_loop():
-                if o_event.type == evdev.ecodes.EV_KEY:
-                    # Pre-parsing the event so it's easier to work with it for our purposes
-                    o_data = evdev.categorize(o_event)
-
-                    # [1/?] Getting the HID byte for the modifier keys
-                    #-------------------------------------------------
-                    # The modifier status will only change when they are pressed (1) or released (0) (not when they are
-                    # hold)
-                    if (o_data.keystate in (0, 1)) and (o_data.keycode in hid_codes.ds_MOD_CODES):
-                        o_hid_keyboard.modifier_set(o_data.keycode, o_data.keystate)
-
-                    # [2/?] Activating or deactivating keys in our HidKeyboard when needed
-                    #---------------------------------------------------------------------
-                    # We only send hid commands when there is a change, so with key-down (1) and key-up (0) events
-                    if o_data.keystate == 0:
-                        o_hid_keyboard.deactivate_key(o_data.keycode)
-                    elif o_data.keystate == 1:
-                        o_hid_keyboard.activate_key(o_data.keycode)
-
-                    # [3/?] When any change occurs, we need to send the HID command
-                    #--------------------------------------------------------------
-                    if o_data.keystate in (0, 1):
-                        if is_debug_enabled:
-                            print(o_hid_keyboard.to_debug_command())
-
-                        if not is_test_mode_enabled:
-                            s_hid_command = o_hid_keyboard.to_hid_command()
-                            try:
-                                o_output_device.write(s_hid_command.encode('utf-8'))
-                            except IOError:
-                                print('--------------------------------------------------')
-                                _get_output_device(o_args['u_output'])
-                                print('--------------------------------------------------')
-                                o_output_device.write(s_hid_command.encode('utf-8'))
-
-        # The o_input_device cannot be read
-        except IOError:
-            print('--------------------------------------------------')
-            o_input_device = _get_input_device(o_args['u_input'])
-            print('--------------------------------------------------')
+if __name__ == "__main__":
+    proxy = ComboDeviceHidProxy(keyboard_in='/dev/input/event3', mouse_in='/dev/input/event4')
+    proxy.combined_event_loop()
--- install.sh
+++ install.sh
@@ -1,17 +1,40 @@
-#!/bin/bash
-echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
-echo "dwc2" | sudo tee -a /etc/modules
-echo "libcomposite" | sudo tee -a /etc/modules
 
-pip3 install evdev
+#!/bin/bash
+
+# Check for superuser privileges
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. Attempting to elevate privileges..."
+    # Re-run the script as root
+    exec sudo bash "$0" "$@"
+fi
+
+# Function to append text to a file if the text doesn't already exist in the file
+append_if_not_exist() {
+  TEXT="$1"
+  FILE="$2"
+  BACKUP="$FILE.bak"
+
+  # Create a backup of the original file
+  cp "$FILE" "$BACKUP"
+
+  if ! grep -q "^$TEXT" "$FILE"; then
+    echo "$TEXT" | tee -a "$FILE" > /dev/null
+  fi
+}
+
+# Check if pip3 is installed, if not, install it
+if ! command -v pip3 &> /dev/null; then
+    apt-get update
+    apt-get install -y python3-pip
+fi
+
+append_if_not_exist "dtoverlay=dwc2" "/boot/config.txt"
+append_if_not_exist "dwc2" "/etc/modules"
+append_if_not_exist "libcomposite" "/etc/modules"
+
+pip3 install evdev adafruit-circuitpython-hid
 
 currentScriptDirectory=$(dirname $(readlink -f $0))
-
-mkdir /tmp/usb_hid
-
-chmod 744 $currentScriptDirectory/usb_hid.sh
-ln -s $currentScriptDirectory/usb_hid.sh /usr/bin/
-ln -s $currentScriptDirectory/usb_hid.service /etc/systemd/system/
 
 mkdir /tmp/bluetooth_2_hid
 
@@ -19,7 +42,11 @@ chmod 744 $currentScriptDirectory/bluetooth_2_hid.py
 ln -s $currentScriptDirectory/bluetooth_2_hid.py /usr/bin/
 ln -s $currentScriptDirectory/bluetooth_2_hid.service /etc/systemd/system/
 
-systemctl enable usb_hid.service
 systemctl enable bluetooth_2_hid.service
 
-reboot
+# Notify user about the reboot
+read -p "The system needs to reboot to complete the installation. Reboot now? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    reboot
+fi
