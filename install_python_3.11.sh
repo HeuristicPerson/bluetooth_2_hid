296a5156693f67eea75cfd1a70d543c5a2e96576
quaxalber <64684396+quaxalber@users.noreply.github.com>
2023-09-24 11:24:48 +0200

    Python build dependencies

--- install_python_3.11.sh
+++ install_python_3.11.sh
@@ -7,7 +7,12 @@ if [[ $EUID -ne 0 ]]; then
     exec sudo bash "$0" "$@"
 fi
 
+echo "Installing Python build dependencies"
+apt-get update
+apt-get install -y wget build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
 PY_VER="3.11.5"
+echo "Installing Python $PY_VER"
 wget "https://www.python.org/ftp/python/$PY_VER/Python-$PY_VER.tgz"
 tar -zxvf "Python-$PY_VER.tgz"
 cd "Python-$PY_VER"
