f76fd089d904e5eeb4f3e2e8de0f8116a9d6371b
Benjamin T <benfred+git@posteo.de>
2023-10-20 13:16:01 +0200

    Bump version

--- bluetooth_2_usb.py
+++ bluetooth_2_usb.py
@@ -39,7 +39,7 @@ from lib.device_link import DeviceLink
 import lib.evdev_adapter as evdev_adapter
 import lib.logger
 
-_VERSION = "0.4.1"
+_VERSION = "0.4.2"
 _VERSIONED_NAME = f"Bluetooth 2 USB v{_VERSION}"
 
 _logger = lib.logger.get_logger()
