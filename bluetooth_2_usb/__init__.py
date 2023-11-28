47e857a8cf517960920447e68209692fe289e640
Benjamin T <benfred+git@posteo.de>
2023-11-28 14:03:51 +0100

    Fix imports

--- bluetooth_2_usb/__init__.py
+++ bluetooth_2_usb/__init__.py
@@ -3,9 +3,9 @@
 # --------------------------------------------------------------------------
 
 from bluetooth_2_usb.args import parse_args
-from bluetooth_2_usb.bluetooth_2_usb.bluetooth_usb_proxy import BluetoothUsbProxy
+from bluetooth_2_usb.bluetooth_usb_proxy import BluetoothUsbProxy
 from bluetooth_2_usb.ecodes import ecodes
 from bluetooth_2_usb.evdev_adapter import is_consumer_key, is_mouse_button
-from bluetooth_2_usb.bluetooth_2_usb.proxy_loop import ProxyLoop
+from bluetooth_2_usb.proxy_loop import ProxyLoop
 import bluetooth_2_usb.logging
 import bluetooth_2_usb.ecodes
