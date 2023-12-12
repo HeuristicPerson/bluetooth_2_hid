c438fe19dbf044d0254f75ddf0c442b789969f5b
Benjamin T <benfred+git@posteo.de>
2023-12-12 09:46:07 +0100

    Remove temporary debug output Bump version

--- bluetooth_2_usb.py
+++ bluetooth_2_usb.py
@@ -13,7 +13,7 @@ from src.bluetooth_2_usb.relay import RelayController, list_readable_devices
 
 
 _logger = get_logger()
-_VERSION = "0.6.6"
+_VERSION = "0.6.7"
 _VERSIONED_NAME = f"Bluetooth 2 USB v{_VERSION}"
 
 
